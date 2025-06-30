0c30865a6777b69193fa1fb1f7b81ed899fa769a
SharifzodaUmar <SharifzodaUmar>
2025-06-30 00:09:48 +0000

    ы

--- src/components/DeviceCheck.jsx
+++ src/components/DeviceCheck.jsx
@@ -4,9 +4,12 @@ const DeviceCheck = () => {
   const [isMobile, setIsMobile] = useState(false)
 
   useEffect(() => {
-    if (window.innerWidth < 1024) {
-      setIsMobile(true)
-    }
+    const isRealMobile =
+      /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
+        navigator.userAgent
+      ) || window.innerWidth < 1024
+
+    setIsMobile(isRealMobile)
   }, [])
 
   if (!isMobile) return null
@@ -14,13 +17,8 @@ const DeviceCheck = () => {
   return (
     <div className="fixed inset-0 bg-black bg-opacity-90 text-white text-center z-50 flex items-center justify-center p-6">
       <div>
-        <h1 className="text-3xl font-bold mb-4">⚠ Бародар ба ноутбук дро ки кор намекна🤣🤣🤣 </h1>
-        <p className="text-lg">Пожалуйста, зайдите на сайт с ноутбука или ПК, чтобы увидеть результат.
-
-
-
-
-        </p>
+        <h1 className="text-3xl font-bold mb-4">⚠ Только для компьютеров</h1>
+        <p className="text-lg">Пожалуйста, зайдите на сайт с ноутбука или ПК, чтобы увидеть результат.</p>
       </div>
     </div>
   )
